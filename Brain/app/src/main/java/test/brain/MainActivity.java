e3628df446a6deef76b5dc8fbd9df748d5647c3a
sunil4092001 <ssunil.kkumar2001@gmail.com>
2021-04-10 17:33:18 +0530

    Login And Signup

---
+++ Brain/app/src/main/java/test/brain/MainActivity.java
@@ -0,0 +1,104 @@
+package test.brain;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase database;
+    private DatabaseReference userref;
+    private static final String USERS="users";
+    private  String email;
+    TextView name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        name = findViewById(R.id.name);
+
+        setSupportActionBar(toolbar);
+
+        userref = FirebaseDatabase.getInstance().getReference();
+        userref = database.getReference(USERS);
+        userref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot ds:snapshot.getChildren()){
+                    if (ds.child("email").getValue().equals(email)){
+                        name.setText(ds.child("Name").getValue(String.class));
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+/*
+    public void logout(View view) {
+        auth.signOut();
+        LoginManager.getInstance().logOut();
+        openlogin();
+
+    }
+
+    private void openlogin() {
+        Intent i = new Intent(MainActivity.this,sign_in.class);
+        startActivity(i);
+    }*/
+}
